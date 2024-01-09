cd6fb92b755b5f3718ca63e77bc0c170a5135a83
ParasSolanki <parassolanki151@gmail.com>
2024-01-09 23:07:03 +0530

    fix: visits not updating and slug API remove caching

--- server/api/link/[slug].get.ts
+++ server/api/link/[slug].get.ts
@@ -2,54 +2,47 @@ import { eq } from "drizzle-orm";
 import { db } from "~/server/db/db";
 import { linksTable } from "~/server/db/schema";
 
-const ONE_HOUR_IN_SECONDS = 60 * 60;
+export default defineEventHandler(async (event) => {
+  const slug = getRouterParam(event, "slug");
 
-export default cachedEventHandler(
-  async (event) => {
-    const slug = getRouterParam(event, "slug");
-
-    if (!slug) {
-      throw createError({
-        message: "Slug is required",
-        status: 400,
-      });
-    }
-
-    const link = await db.transaction(async (tx) => {
-      const [link] = await tx
-        .select({
-          id: linksTable.id,
-          href: linksTable.href,
-          visits: linksTable.visits,
-        })
-        .from(linksTable)
-        .where(eq(linksTable.slug, slug))
-        .limit(1);
-      if (!link) {
-        // TODO: handle link not found case
-        return null;
-      }
-
-      await tx
-        .update(linksTable)
-        .set({
-          visits: link.visits + 1,
-        })
-        .where(eq(linksTable.id, link.id));
-
-      return link;
+  if (!slug) {
+    throw createError({
+      message: "Slug is required",
+      status: 400,
     });
+  }
 
+  const link = await db.transaction(async (tx) => {
+    const [link] = await tx
+      .select({
+        id: linksTable.id,
+        href: linksTable.href,
+        visits: linksTable.visits,
+      })
+      .from(linksTable)
+      .where(eq(linksTable.slug, slug))
+      .limit(1);
     if (!link) {
-      throw createError({
-        message: "Link does not exists",
-        status: 400,
-      });
+      // TODO: handle link not found case
+      return null;
     }
 
-    return { link };
-  },
-  {
-    maxAge: ONE_HOUR_IN_SECONDS,
+    await tx
+      .update(linksTable)
+      .set({
+        visits: link.visits + 1,
+      })
+      .where(eq(linksTable.id, link.id));
+
+    return link;
+  });
+
+  if (!link) {
+    throw createError({
+      message: "Link does not exists",
+      status: 400,
+    });
   }
-);
+
+  return { link };
+});
