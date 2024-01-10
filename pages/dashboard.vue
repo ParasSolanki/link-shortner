53ac5e5dc014f9a48eafd28cf0dbc1641b087dca
ParasSolanki <parassolanki151@gmail.com>
2024-01-10 20:38:10 +0530

    add: dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "app",
+  middleware: ["protected"],
+});
+
+useHead({
+  title: "Dashboard | Link Shortner",
+});
+</script>
+
+<template>
+  <div class="container mx-auto border-b border-b-border px-4 py-6"></div>
+</template>
