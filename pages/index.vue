afd2e09d65b9f323ea2bd1c76c76b8697fa92a38
ParasSolanki <parassolanki151@gmail.com>
2024-01-23 21:18:51 +0530

    add: home page ui

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@ import { z } from "zod";
 import { useMutation } from "~/composables/use-mutation";
 import { toast } from "vue-sonner";
 import { Loader2, Clipboard, ClipboardCheck } from "lucide-vue-next";
+import { buttonVariants } from "~/components/ui/button";
 
 definePageMeta({
   layout: "app",
@@ -61,93 +62,122 @@ function handleCopyLink(link: string) {
 </script>
 
 <template>
-  <div class="max-w-3xl mx-auto px-4 py-20">
-    <Card>
-      <CardHeader>
-        <CardTitle class="text-3xl md:text-4xl lg:text-5xl font-black"
-          >Paste the link to be shortened</CardTitle
+  <section class="py-24 sm:py-40">
+    <div class="max-w-2xl mx-auto px-4 space-y-4 sm:text-center">
+      <h2 class="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+        Shorten Your Links
+      </h2>
+      <p class="text-muted-foreground text-base sm:text-xl mt-4">
+        Make your links shorter and easier to share with our free link
+        shortener.
+      </p>
+
+      <div class="flex items-center space-x-4 sm:justify-center">
+        <NuxtLink to="/signin" :class="buttonVariants({ size: 'lg' })"
+          >Get Started</NuxtLink
         >
-      </CardHeader>
-      <CardContent>
-        <form @submit="onSubmit">
-          <fieldset :disabled="isPending">
-            <FormField v-slot="{ componentField }" name="href">
-              <FormItem>
-                <FormLabel class="uppercase">Link</FormLabel>
-                <div class="flex items-center space-x-4">
-                  <FormControl>
-                    <Input placeholder="Enter link" v-bind="componentField" />
-                  </FormControl>
-                  <Button type="submit" class="shrink-0" :disabled="isPending">
-                    <Loader2
-                      v-if="isPending"
-                      class="mr-1 h-4 w-4 animate-spin"
-                    />
+        <NuxtLink
+          to="/#create"
+          :class="buttonVariants({ size: 'lg', variant: 'outline' })"
+          >Create Short Link</NuxtLink
+        >
+      </div>
+    </div>
+  </section>
 
-                    Shorten Link
-                  </Button>
-                </div>
-                <FormDescription>
-                  Link shortener allows to create a shortened link making it
-                  easy to share.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-          </fieldset>
-        </form>
-
-        <div v-if="data" class="mt-10 space-y-4">
-          <div>
-            <h3 class="text-2xl font-bold">Your shortened Link</h3>
-            <p class="text-muted-foreground text-sm">
-              Copy the short link and share it in messages, texts, posts,
-              websites and other locations.
-            </p>
-          </div>
-
-          <div class="relative">
-            <Input
-              readonly
-              class="pr-12"
-              :default-value="`${BASE_URL}/${data.link.slug}`"
-            />
-
-            <div class="absolute top-1/2 right-0.5 -translate-y-1/2">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger as-child>
+  <section class="bg-muted py-24 sm:py-40">
+    <div class="max-w-3xl mx-auto px-4" id="create">
+      <Card>
+        <CardHeader>
+          <CardTitle class="text-3xl md:text-4xl lg:text-5xl font-black"
+            >Paste the link to be shortened</CardTitle
+          >
+        </CardHeader>
+        <CardContent>
+          <form @submit="onSubmit">
+            <fieldset :disabled="isPending">
+              <FormField v-slot="{ componentField }" name="href">
+                <FormItem>
+                  <FormLabel class="uppercase">Link</FormLabel>
+                  <div class="flex items-center space-x-4">
+                    <FormControl>
+                      <Input placeholder="Enter link" v-bind="componentField" />
+                    </FormControl>
                     <Button
-                      type="button"
-                      variant="ghost"
-                      size="icon"
-                      @click="handleCopyLink(`${BASE_URL}/${data.link.slug}`)"
+                      type="submit"
+                      class="shrink-0"
+                      :disabled="isPending"
                     >
-                      <Clipboard v-if="!copied" class="h-4 w-4" />
-                      <ClipboardCheck v-else class="h-4 w-4 text-green-500" />
+                      <Loader2
+                        v-if="isPending"
+                        class="mr-1 h-4 w-4 animate-spin"
+                      />
+
+                      Shorten Link
                     </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <span>
-                      {{ copied ? "Copied" : "Copy" }}
-                    </span>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+                  </div>
+                  <FormDescription>
+                    Link shortener allows to create a shortened link making it
+                    easy to share.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              </FormField>
+            </fieldset>
+          </form>
+
+          <div v-if="data" class="mt-10 space-y-4">
+            <div>
+              <h3 class="text-2xl font-bold">Your shortened Link</h3>
+              <p class="text-muted-foreground text-sm">
+                Copy the short link and share it in messages, texts, posts,
+                websites and other locations.
+              </p>
+            </div>
+
+            <div class="relative">
+              <Input
+                readonly
+                class="pr-12"
+                :default-value="`${BASE_URL}/${data.link.slug}`"
+              />
+
+              <div class="absolute top-1/2 right-0.5 -translate-y-1/2">
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger as-child>
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="icon"
+                        @click="handleCopyLink(`${BASE_URL}/${data.link.slug}`)"
+                      >
+                        <Clipboard v-if="!copied" class="h-4 w-4" />
+                        <ClipboardCheck v-else class="h-4 w-4 text-green-500" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <span>
+                        {{ copied ? "Copied" : "Copy" }}
+                      </span>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </div>
+            </div>
+            <div class="flex space-x-2">
+              <span class="flex-shrink-0">Long Link:</span>
+              <NuxtLink
+                target="_blank"
+                :to="data.link.href"
+                rel="noreferrer noopener"
+                class="underline underline-offset-4 font-bold text-primary hover:opacity-80"
+                >{{ data.link.href }}</NuxtLink
+              >
             </div>
           </div>
-          <div class="flex space-x-2">
-            <span class="flex-shrink-0">Long Link:</span>
-            <NuxtLink
-              target="_blank"
-              :to="data.link.href"
-              rel="noreferrer noopener"
-              class="underline underline-offset-4 font-bold text-primary hover:opacity-80"
-              >{{ data.link.href }}</NuxtLink
-            >
-          </div>
-        </div>
-      </CardContent>
-    </Card>
-  </div>
+        </CardContent>
+      </Card>
+    </div>
+  </section>
 </template>
